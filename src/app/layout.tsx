27016c3af70d6908088e4edbfe1050ffabaf3e3e
Claire Sylvester <clairesylvester@clairecodes.dev>
2025-06-01 08:54:45 -0700

    remove client side rendering from layout to keep as server side

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
-'use client';
+import { ReactNode } from 'react';
 import '@/styles/globals.scss';
-import { ReactNode, useState } from 'react';
 import { Montserrat } from 'next/font/google';
 
 // components
 import GridOverlayToggle from '@/components/GridOverlayToggle';
 
+// optimized fonts
 const montserrat = Montserrat({
   subsets: ['latin'],
   weight: ['300', '400', '500', '600', '700'],
@@ -14,23 +14,19 @@ const montserrat = Montserrat({
 });
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
-  const [showGrid, setShowGrid] = useState(false);
   const devMode = process.env.NODE_ENV === 'development';
 
   return (
     <html lang="en" className={montserrat.variable}>
       <body>
         {/* DEV GRID TOGGLE */}
-        {devMode && (
-          <GridOverlayToggle
-            active={showGrid}
-            onChange={setShowGrid}
-            className="fixed bottom-8 right-4 z-20"
-          />
-        )}
+        {devMode && <GridOverlayToggle />}
 
         {/* MAIN CONTENT */}
-        <main className={`layout ${showGrid ? 'grid-overlay' : ''}`}>{children}</main>
+        {/* GRID OVERLAY relies on the main layout class */}
+        <main data-grid-overlay className={'layout'}>
+          {children}
+        </main>
       </body>
     </html>
   );
--- src/components/GridOverlayToggle/index.tsx
+++ src/components/GridOverlayToggle/index.tsx
@@ -1,13 +1,8 @@
 'use client';
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import Icon from '@/components/Icon';
 
-type Props = {
-  active: boolean;
-  onChange: (active: boolean) => void;
-  className?: string;
-};
-
+// needs to be hardcoded because tailwind doesn't support dynamic classes
 const breakpoints = [
   { name: 'xs', classes: 'inline sm:hidden' },
   { name: 'sm', classes: 'hidden sm:inline md:hidden' },
@@ -19,43 +14,60 @@ const breakpoints = [
   { name: '4xl', classes: 'hidden 4xl:inline' },
 ];
 
-const GridOverlayToggle: React.FC<Props> = ({ active, onChange, className = '' }) => (
-  <button
-    onClick={() => onChange(!active)}
-    className={`
-      ${className}
-      flex items-center
-      w-32 h-12
-      rounded-lg
-      px-3
-      bg-neu-navy-base
-      shadow-neu-navy-base
-      transition-all duration-200
-    `}
-  >
-    <div className="flex items-end mr-2">
-      <span className="text-white text-display-label font-bold leading-tight">Grid</span>
-      <span className="text-display-caption text-neu-white-darker ml-1">
-        {breakpoints.map(({ name, classes }) => (
-          <span key={name} className={classes}>
-            ({name})
-          </span>
-        ))}
-      </span>
-    </div>
+const GridOverlayToggle: React.FC = () => {
+  const [active, setActive] = useState(false);
+  const mainRef = useRef<HTMLElement | null>(null);
 
-    <div
+  // Get main element reference once on mount
+  useEffect(() => {
+    mainRef.current = document.querySelector('main');
+  }, []);
+
+  // Toggle grid overlay
+  useEffect(() => {
+    if (mainRef.current) {
+      mainRef.current.setAttribute('data-grid-overlay', active ? 'active' : '');
+    }
+  }, [active]);
+
+  return (
+    <button
+      onClick={() => setActive(prev => !prev)}
       className={`
-        flex items-center justify-center
-        w-[36px] h-[36px]
-        rounded-full
-        bg-neu-navy-dark
-        shadow-neu-navy-inset
+        flex items-center
+        w-32 h-12
+        rounded-lg
+        px-3
+        bg-neu-navy-base
+        shadow-neu-navy-base
+        transition-all duration-200
+        fixed bottom-8 right-4 z-20
       `}
     >
-      <Icon type="grid" active={active} size={18} />
-    </div>
-  </button>
-);
+      <div className="flex items-end mr-2">
+        <span className="text-white text-display-label font-bold leading-tight">Grid</span>
+        <span className="text-display-caption text-neu-white-darker ml-1">
+          {breakpoints.map(({ name, classes }) => (
+            <span key={name} className={classes}>
+              ({name})
+            </span>
+          ))}
+        </span>
+      </div>
+
+      <div
+        className={`
+          flex items-center justify-center
+          w-[36px] h-[36px]
+          rounded-full
+          bg-neu-navy-dark
+          shadow-neu-navy-inset
+        `}
+      >
+        <Icon type="grid" active={active} size={18} />
+      </div>
+    </button>
+  );
+};
 
 export default GridOverlayToggle;
