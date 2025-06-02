e182e58dbb6987fd784faba374218e963ac05e6a
Claire Sylvester <clairesylvester@clairecodes.dev>
2025-06-02 09:06:44 -0700

    fix for dev mode grid overlay override

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,16 +14,21 @@ const montserrat = Montserrat({
 });
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
+  // check env vars
   const devMode = process.env.NODE_ENV === 'development';
+  const isGridOverlayOverride = process.env.GRID_OVERLAY_OVERRIDE === 'true';
+
+  // show grid overlay if dev mode is true or if the grid overlay override is true
+  const showGridOverlay = devMode || isGridOverlayOverride;
 
   return (
     <html lang="en" className={montserrat.variable}>
       <body>
         {/* DEV GRID TOGGLE */}
-        {devMode && <GridOverlayToggle />}
+        {showGridOverlay && <GridOverlayToggle />}
 
         {/* MAIN CONTENT */}
-        {/* GRID OVERLAY relies on the main layout class */}
+        {/* GRID OVERLAY relies on the layout class */}
         <main data-grid-overlay className={'layout'}>
           {children}
         </main>
